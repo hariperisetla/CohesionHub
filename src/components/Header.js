85afed1eee8fd2c13db350192c77d17ffd4d5f52
hariperisetla <harinagasai.perisetla@gmail.com>
2023-08-25 13:47:15 +0530

    dynamic color added to header font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
   return (
-    <div className="fixed z-10 flex w-full px-5 py-3 justify-between">
+    <div
+      className={`${
+        pathname === "/" ? "text-white" : "text-black"
+      } fixed z-10 flex w-full px-5 py-3 justify-between`}
+    >
       <Link href="/">
         <h3 className="text-2xl font-bold">CohesionHub</h3>
       </Link>
